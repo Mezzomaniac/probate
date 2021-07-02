dcec977e9bd49622ed11e0e602068a0f18fd9f26
Mezzomaniac <Mezzomaniac@users.noreply.github.com>
2021-07-02 18:53:09 +0800

    formatting

--- probatewebappmain.py
+++ probatewebappmain.py
@@ -4,11 +4,11 @@ from waitress import serve
 from probatewebapp import create_app
 
 def run(test=False):
-	app = create_app(test)
-	if test:
-		app.run(debug=True, use_reloader=False)
-	else:
-		serve(app, port=5000)
+    app = create_app(test)
+    if test:
+        app.run(debug=True, use_reloader=False)
+    else:
+        serve(app, port=5000)
 
 if __name__ == '__main__':
-	run()
+    run()
