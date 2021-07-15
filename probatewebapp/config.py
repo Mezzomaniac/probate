5cbb925d55c712825f0cbec2a8694e14a4bec480
Mezzomaniac <Mezzomaniac@users.noreply.github.com>
2021-07-15 20:58:51 +0800

    incremented version

--- probatewebapp/config.py
+++ probatewebapp/config.py
@@ -18,7 +18,7 @@ def get_password(service, username):
 
 class Config:
     SECRET_KEY = os.urandom(16)
-    VERSION = '0.7.3'
+    VERSION = '0.7.4'
 
     TIMEZONE = datetime.timezone(datetime.timedelta(hours=8), 'AWST')
     
