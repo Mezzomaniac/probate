8995106c43a61b07a8fa8bf8838d810138b46115
Mezzomaniac <themezj@hotmail.com>
2022-08-11 04:06:18 +0000

    disabled updater

--- probatewebapp/__init__.py
+++ probatewebapp/__init__.py
@@ -11,6 +11,7 @@ mail = Mail()
 from .database import init_db, db_last_update, close_db
 from .update import update_db
 
+
 def create_app(test=False, setup=False, years=None):
     app = Flask(__name__)
     config = TestingConfig if test else Config
@@ -22,14 +23,15 @@ def create_app(test=False, setup=False, years=None):
         from . import routes
     #app.teardown_appcontext(close_db)
     app.teardown_request(close_db)
-    
+
     setup_logger(app)
     if app.debug or app.testing:
         app.logger.info('App startup in development/testing mode')
     else:
         app.logger.info('App startup in production mode')
 
-    threading.Thread(target=update_db, name='updater_thread', args=(app,), kwargs={'setup': setup, 'years': years}).start()
+    # Updater is disabled until further notice
+    # because the source database is no longer available
+    #threading.Thread(target=update_db, name='updater_thread', args=(app,), kwargs={'setup': setup, 'years': years}).start()
 
     return app
-
