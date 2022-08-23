7c598f3865ec1a3dbf051333c241dc87efb1828a
Mezzomaniac <themezj@hotmail.com>
2022-08-23 03:01:04 +0000

    fixed error in creating app

--- probatewebappmain.py
+++ probatewebappmain.py
@@ -3,15 +3,17 @@
 from waitress import serve
 from probatewebapp import create_app
 
+
 def run(test=False, setup=False, years=False):
+    app = create_app(test=test, setup=setup, years=years)
     try:
-        app = create_app(test, setup, years)
         if test:
             app.run(debug=True, use_reloader=False)
         else:
             serve(app, port=5000, _quiet=True)
     except Exception:
         app.logger.exception('Unexpected error:')
+
 
 if __name__ == '__main__':
     run()
